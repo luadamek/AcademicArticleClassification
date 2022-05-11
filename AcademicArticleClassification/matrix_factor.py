943f0d5eb72f8aebff214e08ab951ec7332f7cc7
Lukas Adamek <ladamek@gra-login1.graham.sharcnet>
2022-05-11 11:52:47 -0400

    Upload notebooks

--- AcademicArticleClassification/matrix_factor.py
+++ AcademicArticleClassification/matrix_factor.py
@@ -6,26 +6,45 @@ class MatrixFactor:
         self.data = X
         self.masking = masking
         self.latent_factors = latent_factors
-        minimum = np.minimum(self.data)
-        maximum = np.maximum(self.data)
-        self.U = np.random.uniform(minimum, maximum, size=(x.shape[0], latent_factors))
-        self.Z = np.random.uniform(minimum, maximum, size=(latent_factors, x.shape[1]))
+        minimum = np.amin(self.data)
+        maximum = np.amax(self.data)
+        self.U = np.random.uniform(minimum, maximum, size=(self.data.shape[0], latent_factors))
+        self.Z = np.random.uniform(minimum, maximum, size=(latent_factors, self.data.shape[1]))
 
     def update_parameters(self):
+        original_u = np.copy(self.U)
+        original_z = np.copy(self.Z)
+        current_loss = self.calc_loss()
+
         for i in range(0, self.Z.shape[1]):
             M_b = np.einsum("i,ia,ik", self.masking[:,i], self.U, self.U)
-            M_inv= np.lingalg.inverse(M_b)
-            self.Z[:,i] = np.dot(np.dot(M_inv, self.U.transpose), (self.masking * self.data))
+            M_inv= np.linalg.inv(M_b)
+            d1 = np.dot(M_inv, self.U.transpose())
+            result = np.dot(d1 , (self.masking * self.data)[:,i])
+            self.Z[:,i] = result
+
+        new_loss = self.calc_loss()
+        if new_loss > current_loss:
+            self.Z, self.U = original_z, original_u
+
+        original_u = np.copy(self.U)
+        original_z = np.copy(self.Z)
+        current_loss = self.calc_loss()
 
         for i in range(0, self.U.shape[0]):
-            M_a = np.einsum("i,ai,ki", self.making[i,:], self.Z, self.Z)
-            M_inv = np.linalg.inverse(M_a)
-            self.U[i, :] = np.dot(np.dot( (self.masking * self.data), self.Z.transpose), M_inv)
+            M_a = np.einsum("i,ai,ki", self.masking[i,:], self.Z, self.Z)
+            M_inv = np.linalg.inv(M_a)
+            d1 = np.dot( (self.masking * self.data)[i,:], self.Z.transpose())
+            result = np.dot(d1, M_inv)
+            self.U[i, :] = result
+
+        new_loss = self.calc_loss()
+        if new_loss > current_loss:
+            self.Z, self.U = original_z, original_u
 
     def get_predictions(self):
         return np.dot(self.U, self.Z) #recommendations for all users
 
     def calc_loss(self):
         pred = self.get_predictions()
-        axes = [i for i in range(0, len(self.data.shape))]
-        return np.sum(self.masking * (self.data - pred)**2, axis=axes)
+        return np.sum(np.sum(self.masking * (self.data - pred)**2))
